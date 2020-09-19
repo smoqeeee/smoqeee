a60c466faecb48ed16d3c3607249c51fdb2b434b
smoqeeee <71569848+smoqeeee@users.noreply.github.com>
2020-09-19 23:24:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class MyClient(discord.Client):
        print(client.user.name)
        print(client.user.id)
        print("------------")
-       await client.change_presence(game=discord.Game(name="https://shoppy.gg/@LunaticService"))
+       await client.change_presence(game=discord.Activity(name="https://shoppy.gg/@LunaticService"))
 
     async def on_message(self, message):
       if message.author == client.user:
