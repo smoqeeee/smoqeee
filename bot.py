8bc1f9a720ef3b2ec5dfdd5ce131523f0895676a
smoqeeee <71569848+smoqeeee@users.noreply.github.com>
2020-09-20 13:47:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,30 +4,18 @@ import asyncio
 import os
 import requests
 
+client = commands.Bot(command_prefix = '!')
 
-
-
- 
-class MyClient(discord.Client):
- 
-
-    async def on_ready(self):
+@client.event
+  async def on_ready(self):
        print("logged in as smoqeee V1.0")
-       print(client.user.name)
-       print(client.user.id)
-       print("------------")
        await client.change_presence(activity=discord.Streaming(name="shoppy.gg/@LunaticService", url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
 
-    async def on_message(self, message):
-      if message.author == client.user:
-            return
-       
-client = commands.Bot(command_prefix='!')
+    
 @client.command()
 async def info(ctx):
  await ctx.send('test')
         
 
 
-client = MyClient()
 client.run(str(os.environ.get('BOT_TOKEN')))
