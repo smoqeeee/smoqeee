1506d31ed2a8fba9b90c9efa0b86a33a24f5031f
smoqeeee <71569848+smoqeeee@users.noreply.github.com>
2020-10-16 21:20:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,24 @@ client = commands.Bot(command_prefix='l!')
 async def on_ready():
     print('logged in as')
     print(client.user.name)
-    print(client.user.id)
     print('-----')
 
-await client.change_presence(
-    activity=discord.Activity(type=discord.ActivityType.watching, name="users"))
+
+newUserMessage = """ 
+    Welcome
+    to
+    Lunatic Service!
+   
+ """
+
+@client.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    try: 
+    await client.send_message(member, newUserMessage)
+    print("Sent message to " + member.name)
+    except:
+        print("Couldn't message " + member.name)
 
 @client.command()
 async def test(ctx):
@@ -29,16 +42,6 @@ async def commands(ctx):
 @client.command()
 async def lenny(ctx):
     await ctx.send('( ͡° ͜ʖ ͡°) ')
-
-newUserDMMessage = "WELCOME!"
-# Public Welcome
-@client.event
-async def on_member_join(member):
-    print("Recognized that " + member.name + " joined")
-    await client.send_message(member, newUserDMMessage)
-    await client.send_message(discord.Object(id='746442045052157952'), 'Welcome!')
-    print("Sent message to " + member.name)
-    print("Sent message about " + member.name + " to #🌌〕new-people")
 
 
 client.run(str(os.environ.get('BOT_TOKEN')))
