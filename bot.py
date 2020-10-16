0517300c5374f724b79f895832552809f8264f89
smoqeeee <71569848+smoqeeee@users.noreply.github.com>
2020-10-16 21:13:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
+import os
+
 import discord
 from discord.ext import commands
-import asyncio
-import os
-import requests
-import logging
 
 client = commands.Bot(command_prefix='l!')
 
@@ -15,10 +13,11 @@ async def on_ready():
     print(client.user.id)
     print('-----')
 
-newUserDMMessage = "WELCOME!"
-await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="users"))
 
+newUserDMMessage = "WELCOME!"
+
+await client.change_presence(
+    activity=discord.Activity(type=discord.ActivityType.watching, name="users"))
 
 
 @client.command()
@@ -36,10 +35,7 @@ async def lenny(ctx):
     await ctx.send('( ͡° ͜ʖ ͡°) ')
 
 
-
-
-
-#Public Welcome
+# Public Welcome
 @client.event
 async def on_member_join(member):
     print("Recognized that " + member.name + " joined")
@@ -47,7 +43,6 @@ async def on_member_join(member):
     await client.send_message(discord.Object(id='746442045052157952'), 'Welcome!')
     print("Sent message to " + member.name)
     print("Sent message about " + member.name + " to #🌌〕new-people")
-
 
 
 client.run(str(os.environ.get('BOT_TOKEN')))
